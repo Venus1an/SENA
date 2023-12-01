8d9a239ec59507ceb49f1ee782fa907c60691a1e
Venus1an <150965181+Venus1an@users.noreply.github.com>
2023-12-01 12:00:39 -0600

    Add files via upload

---
+++ productos.java
@@ -0,0 +1,19 @@
+public class Productos {
+    public static void main(String[]args){ 
+        Scanner entrada= new Scanner(System.in);
+        int[] Prendas_compradas
+
+        
+        Prendas_compradas= integer.oarseInt (JOptionPane.showuinputdialog("Digite código de factura");
+        
+        char[] letras = new char [Prendas_compradas];
+        Prendas_compradas[4]
+
+        System.out.println ("digite los elementos del arreglo: ");
+        
+        for(int i=0;i<Prendas_compradas;i++) (
+            System.out.println((i+1)))+". Digite un caracter: ");
+            letras[i] = entrada.next().charAt(0);
+        } 
+   }
+    
